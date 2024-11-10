f1f67da0dd58e4395a8fe8528e38035d558f9ee7
Meet Jain <meetjain538@gmail.com>
2024-11-10 22:11:57 +0530

    Update Ultrasonic_Sensor_HC-SR04.ino

--- Ultrasonic_Sensor_HC-SR04.ino
+++ Ultrasonic_Sensor_HC-SR04.ino
@@ -1,26 +1,34 @@
-const int trigPin = 5;
-const int echoPin = 18;
+const int trigPin = 9;
+const int echoPin = 10;
 
-long duration;
-float distance;
+float currentDistance = 0;
+float previousDistance = 0;
+float crackThreshold = 0.5;
+int samplingInterval = 200; 
 
-void setup() 
-{
-  Serial.begin(115200);
+void setup() {
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
-void loop() 
-{
+void loop() {
+  currentDistance = measureDistance();
+  Serial.println(currentDistance); 
+  delay(samplingInterval);
+}
+
+float measureDistance() {
+  long duration;
+  float distance;
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
 
-  distance = duration * 0.034 / 2;
-  Serial.println(distance);
-  delay(100);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * 0.034) / 2;
+  return distance;
 }
